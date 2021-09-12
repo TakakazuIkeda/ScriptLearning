8fdb19665d1f2676a15a56b721662b3722742145
TakakazuIkeda <ikeda.h.academy@gmail.com>
2021-09-12 09:47:09 +0900

    20210921

---
+++ Assets/Seconds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seconds : MonoBehaviour
+{
+    public int Number = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int aaa
+        {
+            int h = (int)(Number / 3600);//秒から時間の値を取得
+            int m = (int)(Number / 60);//秒から分の値を取得
+            int s = (int)(Number);//秒のみを取得
+            return h.ToString("00") + ":" + m.ToString("00") + ":" + s.ToString("00");
+        }
+        Debug.Log("aaa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int Number = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Number % 2 == 0)
+            Debug.Log(Number + "は偶数です。");
+        else if (Number % 2 == 1)
+            Debug.Log(Number + "は奇数です。");
+   
+        if (Number > 0 && Number % 4 == 0 && Number % 100 == 0 && Number % 400 == 0 )
+            Debug.Log("西暦" + Number + "年はうるう年です。2月29日があります");
+        else if (Number > 0 && Number % 4 == 1)
+            Debug.Log("西暦" + Number + "年はうるう年ではなく、2月29日がありません");
+        else if (Number > 0 && Number % 4 == 2)
+            Debug.Log("西暦" + Number + "年はうるう年ではなく、2月29日がありません");
+        else if (Number > 0 && Number % 4 == 3)
+            Debug.Log("西暦" + Number + "年はうるう年ではなく、2月29日がありません");
+        else if (Number > 0 && Number % 100 == 0)
+            Debug.Log("西暦" + Number + "年はうるう年ではなく、2月29日がありません");
+        else
+            Debug.Log(Number + "は自然数ではありません。");
+
+        UnityEngine.Debug.Log(Number + "年間は" + Number * 365 + "日です");
+        UnityEngine.Debug.Log(Number + "年間は" + Number * 365 * 24 + "時間です");
+        UnityEngine.Debug.Log(Number + "年間は" + Number * 365 * 24 * 60 + "分です");
+        UnityEngine.Debug.Log(Number + "年間は" + Number * 365 * 24 * 60 * 60 + "秒です");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
